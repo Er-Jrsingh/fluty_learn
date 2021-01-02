30636ad20599028b97e5e478984088109763240d
Jitesh Singh <jitust98@gmail.com>
2021-01-02 22:38:54 +0530

    Fluty Form Tutoriral

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
+// ignore: implementation_imports
+import 'package:flutter/src/widgets/framework.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner:false,
-      title:"MaterilApp Title",
-      home:Scaffold(
-          appBar: AppBar(title:Text("Hello World!!!"),),
-          body:Material(
-              color:Colors.tealAccent,
-              child:Center(
-                child:Text(
-                  "Welcome To Scaffold Tutorial",
-                  textDirection:TextDirection.ltr,
-                  style:TextStyle(
-                    color:Colors.white,
-                    fontSize: 25.0,
-                  )
-                )
-              ),
-            )
-        )
-     ));
+    
+    MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      title: "Simple Form",
+
+      home:SimpleForm()
+      )
+  );
+}
+
+
+class SimpleForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    
+    return _SimpleFormState();
+  }
+
+
+}
+
+class _SimpleFormState extends State<SimpleForm>{
+
+String name=" ";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title:Text("Simple Form",)),
+      body:Container(
+        margin: EdgeInsets.all(30.0),
+        child:Column(
+        children: <Widget>[
+          TextField(
+            onSubmitted:(String userName){
+              setState(() {
+                name= userName;
+               });          
+              },
+             ),
+            Padding(padding: EdgeInsets.all(20.0),
+            child:Text(
+              "Name is $name",
+              style:TextStyle(fontSize: 25.0)
+            )),
+
+        ],
+        ),),
+    );
+  }
 }
