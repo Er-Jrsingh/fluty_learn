1660b9888edab439fbca549b2fd9854d1bbd1464
Jitesh Singh <jitust98@gmail.com>
2021-01-02 22:33:26 +0530

    Scaffold Tut

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Material(
-      color:Colors.deepOrange,
-      child: Center(
-          child: Text(
-              "I Am Jitu Thakur",
-              textDirection:TextDirection.ltr,
-              style: TextStyle(color: Colors.white , fontSize: 30,fontWeight:FontWeight.bold,)
-          )
-      ),
-    )
-  );
+    MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title:"MaterilApp Title",
+      home:Scaffold(
+          appBar: AppBar(title:Text("Hello World!!!"),),
+          body:Material(
+              color:Colors.tealAccent,
+              child:Center(
+                child:Text(
+                  "Welcome To Scaffold Tutorial",
+                  textDirection:TextDirection.ltr,
+                  style:TextStyle(
+                    color:Colors.white,
+                    fontSize: 25.0,
+                  )
+                )
+              ),
+            )
+        )
+     ));
 }
