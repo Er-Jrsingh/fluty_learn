fb683d526f6b3fe966d023c8cfa4b7fdff118422
Jitesh Singh <jitust98@gmail.com>
2021-01-03 13:30:25 +0530

    Bottom Nav Tut

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:fluty_learn/samplePages.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "Bottom Navigation",
+    home: BottomMenu(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
- 
+class BottomMenu extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomMenuState();
+  }
+}
+
+class _BottomMenuState extends State<BottomMenu> {
+  var _pagesData = [HomePage(), AboutPage(), ServicesPage()];
+  int _selectedItem = 0;
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        "Bottom Navigation",
+      )),
+      body: Center(
+        child: _pagesData[_selectedItem],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.cleaning_services), label: "Services"),
+          ],
+          currentIndex: _selectedItem,
+          onTap: (setValue) {
+            setState(() {
+              _selectedItem = setValue;
+            });
+          }),
+    );
   }
 }
---
+++ lib/samplePages.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("HomePage Data"),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("AboutPage Data"),
+      ),
+    );
+  }
+}
+
+class ServicesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Services  Data"),
+      ),
+    );
+  }
+}
