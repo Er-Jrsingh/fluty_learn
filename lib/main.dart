f747884871d4238043b4fca0b29e55f54468c916
Jitesh Singh <jitust98@gmail.com>
2021-01-03 11:33:24 +0530

    Table Tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
- 
-  @override
-  Widget build(BuildContext context) {
-
+void main(){
+    // runApp( new MyTable ());
+    // runApp( MyTable ());
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner:false,
+        title:"MaterilApp Title",
+        
+             home: Scaffold(
+                    appBar: AppBar(title:Text("Table Tutorial")),
+                    body:  Container(
+                margin:EdgeInsets.all(10.0),
+                child:Table(
+                  border: TableBorder.all(),
+                  
+                  children:[
+                    TableRow(
+                    children: [
+                      Text("First Name",style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,),),
+                      Text("Last Name" , style:TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,),)
+                    ]
+                  ),
+                    TableRow(
+                    children: [
+                      Text("Jitesh",style: TextStyle(fontSize: 15.0,),),
+                      Text("Thakur",style: TextStyle(fontSize: 15.0,))
+                    ]
+                    ),
+                     TableRow(
+                    children: [
+                      Text("Utkarsh",style: TextStyle(fontSize: 15.0,),),
+                      Text("Tiwari",style: TextStyle(fontSize: 15.0,))
+                    ]
+                    ),
+                     TableRow(
+                    children: [
+                      Text("Shudhanshu",style: TextStyle(fontSize: 15.0,),),
+                      Text("Singh",style: TextStyle(fontSize: 15.0,))
+                    ]
+                    )]
+                )
+                )
+      )
+    )
+    );
   }
-}
+  
