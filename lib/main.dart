e4f2b565d37b1059ec3fe1e00034760d1fdd823c
Jitesh Singh <jitust98@gmail.com>
2021-01-03 11:41:25 +0530

    Organiese the fluty table Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:fluty_learn/table_screen/table_body.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
- 
+void main() =>runApp( MyTable ());
+
+/*
+void main(){
+    runApp( new MyTable ());
+    runApp( MyTable ());
+  }
+  */
+class MyTable extends  StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-
+    return  MaterialApp(
+        debugShowCheckedModeBanner:false,
+        title:"MaterilApp Title",
+        
+             home: Scaffold(
+                    appBar: AppBar(title:Text("Table Tutorial")),
+                    body:  MyTabBody()
+      )
+    );
   }
 }
