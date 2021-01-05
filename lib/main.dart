80ad37ebce0cc658f6bfb84eb7b9c2b30eb1680f
Jitesh Singh <jitust98@gmail.com>
2021-01-05 21:31:15 +0530

    Type8 Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -4,105 +4,158 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     title: "Buttons",
-     color: Colors.indigo,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Buttons",
+      color: Colors.indigo,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.limeAccent,
-          title:Text("All Buttons" , style: TextStyle(color: Colors.indigoAccent),),
+          title: Text(
+            "All Buttons",
+            style: TextStyle(
+              color: Colors.indigo,
+              fontWeight: FontWeight.bold,
+              fontFamily: "poppins",
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          centerTitle: true,
 
-           /*  Icon Button   */
+          /*  Icon Button   */
 
-          actions:[
-            IconButton(icon: Icon(Icons.menu,color:Colors.tealAccent),onPressed: (){print("I am Icon Button");},),
-          ]
-       
+          actions: [
+            IconButton(
+              icon: Icon(Icons.menu, color: Colors.indigo),
+              onPressed: () {
+                print("I am Icon Button");
+              },
+            ),
+          ],
         ),
-        body:Padding(
-          padding: const EdgeInsets.only(top:25.0,left: 100),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 25.0, left: 100),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              /* flat Icon button */
+
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Colors.tealAccent,
+                onPressed: () {
+                  print(
+                    "I am flat button",
+                  );
+                },
+                child: Text(
+                  "Flat Button",
+                ),
+              ),
+              SizedBox(height: 10),
 
               /* flat Icon button */
 
-            FlatButton(
-              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),
-              color: Colors.tealAccent,
-              onPressed: (){
-              print("I am flat button");
-            }, child: Text("Flat Button")),
-            SizedBox(height:10),
+              FlatButton.icon(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.tealAccent,
+                onPressed: () {
+                  print(
+                    "I am flat Icon button",
+                  );
+                },
+                icon: Icon(Icons.settings),
+                label: Text(
+                  "Flat Icon Button",
+                ),
+              ),
+              SizedBox(height: 10),
 
-              /* flat Icon button */
+              /* RaisedButton*/
 
-            FlatButton.icon(
-              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),
-              color: Colors.tealAccent, 
-              onPressed: (){print("I am flat Icon button");},
-             icon: Icon(Icons.settings), label: Text("Flat Icon Button")),
-             SizedBox(height:10),
-
-             /* RaisedButton*/
-
-             RaisedButton(
-              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),
-              color: Colors.tealAccent,
-              onPressed: (){
-              print("I am Raised button");
-            }, child: Text("Raised Button")),
-            SizedBox(height:10),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.tealAccent,
+                onPressed: () {
+                  print("I am Raised button");
+                },
+                child: Text("Raised Button"),
+              ),
+              SizedBox(height: 10),
 
               /* Raised Icon Button*/
 
               RaisedButton.icon(
-               icon: Icon(Icons.ac_unit,color:Colors.white),
-               shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(80)),
-               elevation: 20,
-               color: Colors.tealAccent,
-               label:Text("Raised Icon Button"),
-               onPressed: (){
-                 print("I am Raised Icon button");
-               }),
-                SizedBox(height:10),
+                icon: Icon(Icons.ac_unit, color: Colors.white),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(80),
+                ),
+                elevation: 20,
+                color: Colors.tealAccent,
+                label: Text(
+                  "Raised Icon Button",
+                ),
+                onPressed: () {
+                  print(
+                    "I am Raised Icon button",
+                  );
+                },
+              ),
+              SizedBox(height: 10),
 
-               /* InkWell Button*/
+              /* InkWell Button*/
 
-                     InkWell(
-                 onTap: (){print("I am inkwell ");},
-                                child: Container(
-                   height:70,width:150,
-                   color:Colors.tealAccent,
-                   child:Center(child: Text("Hello I am Inkwell"))
-                 ),
-               ),
-               SizedBox(height:10),
+              InkWell(
+                onTap: () {
+                  print("I am inkwell ");
+                },
+                child: Container(
+                    height: 70,
+                    width: 150,
+                    color: Colors.tealAccent,
+                    child: Center(child: Text("Hello I am Inkwell"))),
+              ),
+              SizedBox(height: 10),
 
-               
-               /* GestureDetector Button*/
+              /* GestureDetector Button*/
 
-               GestureDetector(
-            onTap: (){print("I am Tapped GestureDetector");},
-            onHorizontalDragEnd:(value)=>print(value) ,
-            onVerticalDragDown: (value)=>print(value),
-             child:Container(
-               height: 70,width: 80,
-               decoration: BoxDecoration(
-                 color:Colors.tealAccent,
-                 shape:BoxShape.circle
-               ),
-               child: Center(child:Text("Gestures")),)
-           )
-            
-          ],),
-        )
+              GestureDetector(
+                onTap: () {
+                  print("I am Tapped GestureDetector");
+                },
+                onHorizontalDragEnd: (value) => print(value),
+                onVerticalDragDown: (value) => print(value),
+                child: Container(
+                  height: 70,
+                  width: 80,
+                  decoration: BoxDecoration(
+                      color: Colors.tealAccent, shape: BoxShape.circle),
+                  child: Center(child: Text("Gestures")),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /*   FloatingActionButton  */
+
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            print("I Am Floating Action Button");
+          },
+          label: Text('FloatActiBtn'),
+          icon: Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
       ),
-   );
+    );
   }
-
 }
