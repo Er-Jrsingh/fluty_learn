374d09a05c93e61db5efb51291b951feda92d1ac
Jitesh Singh <jitust98@gmail.com>
2021-01-05 12:38:46 +0530

    Theme Color change

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner:false,
-      title:"MaterilApp Title",
-      home:Scaffold(
-          appBar: AppBar(title:Text("Hello World!!!"),),
-          body:Material(
-              color:Colors.tealAccent,
-              child:Center(
-                child:Text(
-                  "Welcome To Scaffold Tutorial",
-                  textDirection:TextDirection.ltr,
-                  style:TextStyle(
-                    color:Colors.white,
-                    fontSize: 25.0,
-                  )
-                )
+      debugShowCheckedModeBanner: false,
+      title: "MaterilApp Title",
+      theme: ThemeData(primaryColor: Colors.teal[400]),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 10,
+          title: Text(
+            "Hello World !!!",
+          ),
+          centerTitle: true,
+        ),
+        body: Material(
+          color: Colors.tealAccent,
+          child: Center(
+            child: Text(
+              "Welcome To Scaffold Tutorial",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
               ),
-            )
-        )
-     ));
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
