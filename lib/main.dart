5e6ffaa46e6c4eb87f226c163e68bbbb35f39e48
Jitesh Singh <jitust98@gmail.com>
2021-01-05 14:58:33 +0530

    Add Model Bottom Sheet & also Use the Constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,72 @@
-import 'package:flutter/material.dart';
+import'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "MaterilApp Title",
-      theme: ThemeData(primaryColor: Colors.teal[200]),
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          title: Text(
-            "Hello World !!!",
-          ),
-          centerTitle: true,
-        ),
-        body: Material(
-          color: Colors.tealAccent,
-          child: Center(
-            child: Text(
-              "Welcome To Scaffold Tutorial",
-              // textDirection: TextDirection.,
-              style: TextStyle(
-                color: Colors.deepOrange,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.home, color: Colors.pinkAccent),
-          backgroundColor: Colors.white,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        drawer: Drawer(
-          elevation: 5,
-          child: Column(
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text("Jitesh Thakur"),
-                accountEmail: Text("Jitust98@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.pinkAccent,
-                  child: Text("Jitu"),
+      theme: ThemeData(primaryColor:Colors.teal[200]),
+      home: HomePage(middle: 'Please Update Your Profile',),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  final String middle;
+
+  const HomePage({Key key, this.middle}) : super(key: key);     // Setter 
+  
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     body: Center(child:Text('${widget.middle}')),
+     floatingActionButton: FloatingActionButton(
+       onPressed: (){_bottomsheet(context);},
+       backgroundColor: Colors.teal[400],
+       child: Icon(Icons.edit),
+     ),
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+   );
+  }
+  void _bottomsheet(BuildContext context){
+    showModalBottomSheet(context:context, builder: (contxt){
+      return Container(
+          color: Color(0xFF737373),
+              child: Container(
+                decoration: BoxDecoration(
+                  color:Theme.of(context).canvasColor,
+                  borderRadius:BorderRadius.only(
+                    topLeft:Radius.circular(30),
+                    topRight:Radius.circular(30)
+                  )
                 ),
-              ),
-              ListTile(
-                focusColor: Colors.orangeAccent,
-                title: Text("Enginner"),
-                leading: Icon(Icons.engineering),
-                tileColor: Colors.tealAccent[100],
-                // trailing: Text("Enginner"),
-              ),
-              ListTile(
-                focusColor: Colors.orangeAccent,
-                title: Text("Philanthropist"),
-                leading: Icon(Icons.people_alt),
-                tileColor: Colors.tealAccent[200],
-                // trailing: Text("Enginner"),
-              ),
-              ListTile(
-                focusColor: Colors.orangeAccent,
-                title: Text("AI Enthusiast"),
-                leading: Icon(Icons.developer_board),
-                tileColor: Colors.tealAccent[100],
-                // trailing: Text("Enginner"),
-              ),
-            ],
-          ),
+          height: 200,
+          child: Column(children: [
+            ListTile(
+              leading:Icon(Icons.edit),
+              title: Text('Update your name'),
+              onTap: (){Navigator.pop(context);},
+            ),
+            ListTile(
+              leading:Icon(Icons.image),
+              title: Text('Update your pic'),
+            ),
+            ListTile(
+              leading:Icon(Icons.settings),
+              title: Text('Setting'),
+            )
+          ],),
         ),
-        persistentFooterButtons: [
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.greenAccent,
-            elevation: 20,
-            child: Icon(Icons.print, color: Colors.indigoAccent),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.greenAccent,
-            elevation: 20,
-            child: Icon(Icons.privacy_tip, color: Colors.indigoAccent),
-          ),
-        ],
-      ),
-    ),
-  );
+      );
+    });
+  }
 }
