daf47d48bb64d009f1b67ed5658a5e0dd6f0482e
Jitesh Singh <jitust98@gmail.com>
2021-01-05 20:00:46 +0530

    Type 1 - Flat Button

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
- 
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.limeAccent,
+          title:Text("All Buttons"),
+       
+        ),
+        body:Padding(
+          padding: const EdgeInsets.only(top:25.0,left: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            FlatButton(
+              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20)),
+              color: Colors.tealAccent,
+              onPressed: (){
+              print("I am flat button");
+            }, child: Text("Flat Button")),
+            SizedBox(height:10),
+            
+          ],),
+        )
+      ),
+   );
   }
+
 }
