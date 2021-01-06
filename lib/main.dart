7ec11c19a4567383fe247403d86cb021f329c7c4
Jitesh Singh <jitust98@gmail.com>
2021-01-06 12:36:42 +0530

    Snack Bar Problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Snacky",
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Snacky",
+        ),
+        backgroundColor: Colors.indigoAccent,
+      ),
 
+      /* Snack Bar Problem  */
+
+      body: Center(
+        child: FlatButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: Colors.indigoAccent,
+          onPressed: () {
+            final snackbar = SnackBar(
+              content: Text("Hii !! Snacky !!!"),
+            );
+            Scaffold.of(context).showSnackBar(snackbar);
+          },
+          child: Text(
+            "Flat Button",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
