c69c3d366083f60b8eefdc6c4ea6c748db8b9809
Jitesh Singh <jitust98@gmail.com>
2021-01-06 18:41:50 +0530

    Simple In One Page With Back Arrow

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,72 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Splash Screen",
+      home: SplashScreen()));
 }
 
-class MyApp extends StatelessWidget {
- 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Center(
+        child: Text(
+          " Welcome DoDo !! ",
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Details Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              " HELLO World  !!!! ",
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
