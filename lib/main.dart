d8fbe04a652f994708e0575fd4fdb2eeb8be995b
Jitesh Singh <jitust98@gmail.com>
2021-01-06 14:23:52 +0530

    Add Text Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Material(
-      color:Colors.deepOrange,
-      child: Center(
-          child: Text(
-              "I Am Jitu Thakur",
-              textDirection:TextDirection.ltr,
-              style: myFont()
-          )
-      ),
-    )
-  );
+  runApp(Material(
+    color: Colors.deepOrange,
+    child: Center(
+        child: Text("I Am Jitu Thakur",
+            textDirection: TextDirection.ltr, style: myFont())),
+  ));
 }
 
-TextStyle myFont(){
-  return TextStyle(fontFamily: 'Anurati',color: Colors.white , fontSize: 30,fontWeight:FontWeight.bold,);
+TextStyle myFont() {
+  return TextStyle(
+      fontFamily: 'Anurati',
+      color: Colors.white,
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+      background: Paint()
+        ..color = Colors.tealAccent
+        ..style = PaintingStyle.stroke,
+      letterSpacing: 10.0,
+      wordSpacing: 5.0,
+      fontStyle: FontStyle.italic,
+      decoration: TextDecoration.overline,
+      decorationColor: Colors.white);
 }
