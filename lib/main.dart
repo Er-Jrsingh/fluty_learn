218dfa5f9765654a7cf354f9110fcf0d3a82079e
Jitesh Singh <jitust98@gmail.com>
2021-01-06 19:20:56 +0530

    Organized Code In Two Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'Splash.dart';
 
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Splash Screen",
       home: SplashScreen()));
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreen createState() => _SplashScreen();
-}
-
-class _SplashScreen extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    Timer(
-      Duration(seconds: 2),
-      () => Navigator.of(context).pushReplacement(  MaterialPageRoute(
-          builder: (context) => Home(),)
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
-      body: Center(
-        child: Text(
-          " Hello DoDo !! ",
-          style: TextStyle(
-            fontSize: 40.0,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 class Home extends StatelessWidget {
