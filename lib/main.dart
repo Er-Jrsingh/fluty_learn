13017defb7f5106390be69164a4e107baa5728bb
Jitesh Singh <jitust98@gmail.com>
2021-01-09 22:12:36 +0530

    Login & Fetch Json Data , Using Http Note:- Login Form Not Validate

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:awesomeapp/pages/home_page.dart';
+import 'package:awesomeapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
- 
-  @override
-  Widget build(BuildContext context) {
-
-  }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Login App",
+      home: LoginPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      },
+    ),
+  );
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Dynamic Data"),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+    );
+  }
+}
