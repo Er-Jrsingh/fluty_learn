b211b4e204e7b91f762f596362df5e5048eb51b0
Jitesh Singh <jitust98@gmail.com>
2021-01-10 20:29:48 +0530

    LogOut Functionality Using Shared Preference(Persistant Storage) Note:- Id,Pw Not Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
+import 'package:flutty_learn/pages/home_page.dart';
+import 'package:flutty_learn/pages/login_page.dart';
+import 'package:flutty_learn/utils/Constants.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
- 
-  @override
-  Widget build(BuildContext context) {
+  Constants.prefs = await SharedPreferences.getInstance();
 
-  }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Logout Functinality",
+      home: Constants.prefs.getBool("loggedIn") == true
+          ? HomePage()
+          : LoginPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      },
+    ),
+  );
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutty_learn/utils/Constants.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Home Page"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                Constants.prefs.setBool("loggedIn", false);
+                Navigator.pushReplacementNamed(context, "/login");
+              })
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+    );
+  }
+}
