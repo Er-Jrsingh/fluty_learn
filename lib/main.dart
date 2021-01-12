390fc74559c3346469adc7552b80f97721b36ac1
Jitesh Singh <jitust98@gmail.com>
2021-01-12 12:50:32 +0530

    Future concept

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "FutureAsync",
+      home: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
- 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("FutureAsync")),
+      body: Center(
+        child: Container(
+
+          child: RaisedButton(
+            onPressed: () {fun1();},
+            child:Text("Test")
+            
+          ),
+        ),
+      ),
+    );
+  }
+
+  void fun1() {
+    print("Hola ! I'm Fun1 ");
+    fun2();
+  }
+
+  void fun2() {
+    Future.delayed(Duration(seconds: 5),(){
+    print("Hola! I'm Fun2 ");
+    });
+
+    print("Hola! Hola! Hola! I'm Fun2");
 
   }
 }
