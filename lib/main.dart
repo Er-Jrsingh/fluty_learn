e7fa152634d3807b9c1ab34d2a2989b58824d953
Jitesh Singh <jitust98@gmail.com>
2021-01-12 19:40:15 +0530

    Two Container Takes 50% - 50% Width

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title:"MediaQuery",
+    home:MyApp(),),);
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MediaQuery"),
+      ),
+      body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width*0.5,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width*0.5,
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+              ),
+            ),
+          ]),
+    );
   }
 }
