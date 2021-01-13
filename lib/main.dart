48473f60dcec6203c38e1a8fbbea2265f0b85bc7
Jitesh Singh <jitust98@gmail.com>
2021-01-13 11:16:13 +0530

    Add Basic Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "FlutyLearn",
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FlutyLearn"),
+      ),
+    );
   }
 }
