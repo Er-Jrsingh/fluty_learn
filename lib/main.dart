4ac57bcfbf998c2a494ec29ee66f83ad7a89c496
Jitesh Singh <jitust98@gmail.com>
2021-01-13 11:08:43 +0530

    Different Layout For Portarait & Landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Responsive",
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: Text("Responsive"),
+        ),
+        body: OrientationBuilder(builder: (context, orientaion) {
+          if (orientaion == Orientation.portrait) {
+            return potrait();
+          } else {
+            return landscapes();
+          }
+        }));
+  }
 
+  Widget potrait() {
+    return Container(
+        decoration: BoxDecoration(color: Colors.pink[500]),
+        child: Center(child: Text("Hola! I'm Potrait")));
+  }
+
+  Widget landscapes() {
+    return Container(
+        decoration: BoxDecoration(color: Colors.pinkAccent),
+        child: Center(child: Text("Hola! I'm Landscape")));
   }
 }
