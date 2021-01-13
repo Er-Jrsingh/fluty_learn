232548d690fa5f6d7354887b098142a4d59f16b0
Jitesh Singh <jitust98@gmail.com>
2021-01-13 21:25:13 +0530

    Carousel Using carousel_slider: ^2.3.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(
@@ -17,6 +18,29 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("FlutyLearn"),
       ),
+      body: Center(
+        child: CarouselSlider(
+          options: CarouselOptions(
+            autoPlay: true,
+            enlargeCenterPage: true,
+            height: 400.0,
+          ),
+          items: _source.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: i),
+                  child: Center(child: Text("Hello!!")),
+                );
+              },
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
+
+  final List _source = [Colors.pinkAccent, Colors.redAccent, Colors.deepOrange];
 }
