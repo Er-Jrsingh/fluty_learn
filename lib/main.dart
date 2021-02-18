1bb4368ed405cf5b3c4fb8afd9ddeb71b7a0e757
JITESH SINGH <Jitust98@gmail.com>
2021-02-18 11:41:28 +0530

    Add aspect Ratio

--- lib/main.dart
+++ lib/main.dart
@@ -20,33 +20,33 @@ class HomePage extends StatelessWidget {
           alignment: Alignment.bottomRight,
           child: Container(
             color: Colors.indigoAccent,
-            width: 200,
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  )
-                ],
+            child: AspectRatio(
+              aspectRatio: 1 / 4,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      color: Colors.pink,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                ),
               ),
             ),
           ),
