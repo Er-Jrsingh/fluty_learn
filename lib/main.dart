52828809a3251aca33a4d066cd03c80f2745f975
JITESH SINGH <Jitust98@gmail.com>
2021-02-18 14:21:17 +0530

    Add Baseline Widget To Align Text

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Material(
-      color:Colors.deepOrange,
-      child: Center(
-          child: Text(
-              "I Am Jitu Thakur",
-              textDirection:TextDirection.ltr,
-              style: TextStyle(color: Colors.white , fontSize: 30,fontWeight:FontWeight.bold,)
-          )
-      ),
-    )
+    MaterialApp(
+      home: Homi(),
+    ),
   );
+}
+
+class Homi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Row(children: [
+          Center(
+            child: Text(
+              "H",
+              style:TextStyle(color: Colors.white,fontSize: 40),
+            ),
+          ),
+          Baseline(
+            baselineType: TextBaseline.alphabetic,
+            baseline: 50,
+            child: Text(
+              "2",
+               style:TextStyle(color: Colors.white,fontSize: 30),
+            ),
+          ),
+          Text(
+              "O",
+              style:TextStyle(color: Colors.white,fontSize: 40),
+            ),
+        ],),
+      ),
+    );
+  }
 }
