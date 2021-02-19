fb6bf8b3c6f9885dd079a234139c8e85b9dcbee5
JITESH SINGH <Jitust98@gmail.com>
2021-02-19 14:43:17 +0530

    Wrap Widget Properties

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: "FlutyLearn",
       theme: ThemeData(
-        backgroundColor: Colors.deepPurple
-        ,fontFamily: "cursive",
-        ),
+        backgroundColor: Colors.deepPurple,
+        fontFamily: "cursive",
+      ),
       home: MyApp(),
     ),
   );
@@ -18,9 +18,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("FlutyLearn"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("FlutyLearn"),
+        ),
+        backgroundColor:Colors.white,
+        body: Center(
+          child: Wrap(
+
+            direction: Axis.vertical,
+            alignment: WrapAlignment.start,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            verticalDirection: VerticalDirection.up,
+            spacing: 30.0,
+            runSpacing: 20.0,
+            children: [
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn1"),
+                color: Colors.pink,
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn2"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn3"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn4"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn5"),
+              ),
+               RaisedButton(
+                onPressed: null,
+                child: Text("Btn1"),
+                color: Colors.pink,
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn2"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn3"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn4"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn5"),
+              ),
+               RaisedButton(
+                onPressed: null,
+                child: Text("Btn1"),
+                color: Colors.pink,
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn2"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn3"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn4"),
+              ),
+              RaisedButton(
+                onPressed: null,
+                child: Text("Btn5"),
+              ),
+            ],
+          ),
+        ));
   }
 }
