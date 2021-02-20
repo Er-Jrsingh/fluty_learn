65f28b3727522671c740822b70d5c80ec143b75c
JITESH SINGH <Jitust98@gmail.com>
2021-02-20 12:11:59 +0530

    Type11 Outline  Button

--- lib/main.dart
+++ lib/main.dart
@@ -38,128 +38,154 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 10.0, left: 100),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /* flat Icon button */
 
-              /* flat Icon button */
-
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.tealAccent,
-                onPressed: () {
-                  print(
-                    "I am flat button",
-                  );
-                },
-                child: Text(
-                  "Flat Button",
-                ),
-              ),
-              SizedBox(height: 2),
-
-              /* flat Icon button */
-
-              FlatButton.icon(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                color: Colors.tealAccent,
-                onPressed: () {
-                  print(
-                    "I am flat Icon button",
-                  );
-                },
-                icon: Icon(Icons.settings),
-                label: Text(
-                  "Flat Icon Button",
-                ),
-              ),
-              SizedBox(height: 2),
-
-              /* RaisedButton*/
-
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                color: Colors.tealAccent,
-                onPressed: () {
-                  print("I am Raised button");
-                },
-                child: Text("Raised Button"),
-              ),
-              SizedBox(height: 2),
-
-              /* Raised Icon Button*/
-
-              RaisedButton.icon(
-                icon: Icon(Icons.ac_unit, color: Colors.white),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(80),
-                ),
-                elevation: 10,
-                color: Colors.tealAccent,
-                label: Text(
-                  "Raised Icon Button",
-                ),
-                onPressed: () {
-                  print(
-                    "I am Raised Icon button",
-                  );
-                },
-              ),
-              SizedBox(height: 8),
-
-              /* InkWell Button*/
-
-              InkWell(
-                onTap: () {
-                  print("I am inkwell ");
-                },
-                child: Container(
-                    height: 70,
-                    width: 150,
+                  FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
                     color: Colors.tealAccent,
-                    child: Center(child: Text("Hello I am Inkwell"))),
+                    onPressed: () {
+                      print(
+                        "I am flat button",
+                      );
+                    },
+                    child: Text(
+                      "Flat Button",
+                    ),
+                  ),
+                  SizedBox(height: 2),
+
+                  /* flat Icon button */
+
+                  FlatButton.icon(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: Colors.tealAccent,
+                    onPressed: () {
+                      print(
+                        "I am flat Icon button",
+                      );
+                    },
+                    icon: Icon(Icons.settings),
+                    label: Text(
+                      "Flat Icon Button",
+                    ),
+                  ),
+                  SizedBox(height: 2),
+
+                  /* RaisedButton*/
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: Colors.tealAccent,
+                    onPressed: () {
+                      print("I am Raised button");
+                    },
+                    child: Text("Raised Button"),
+                  ),
+                  SizedBox(height: 2),
+
+                  /* Raised Icon Button*/
+
+                  RaisedButton.icon(
+                    icon: Icon(Icons.ac_unit, color: Colors.white),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80),
+                    ),
+                    elevation: 10,
+                    color: Colors.tealAccent,
+                    label: Text(
+                      "Raised Icon Button",
+                    ),
+                    onPressed: () {
+                      print(
+                        "I am Raised Icon button",
+                      );
+                    },
+                  ),
+                  SizedBox(height: 8),
+
+                  /* InkWell Button*/
+
+                  InkWell(
+                    onTap: () {
+                      print("I am inkwell ");
+                    },
+                    child: Container(
+                        height: 70,
+                        width: 150,
+                        color: Colors.tealAccent,
+                        child: Center(child: Text("Hello I am Inkwell"))),
+                  ),
+                  SizedBox(height: 8),
+
+                  /* GestureDetector Button*/
+
+                  GestureDetector(
+                    onTap: () {
+                      print("I am Tapped GestureDetector");
+                    },
+                    onHorizontalDragEnd: (value) => print(value),
+                    onVerticalDragDown: (value) => print(value),
+                    child: Container(
+                      height: 70,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          color: Colors.tealAccent, shape: BoxShape.circle),
+                      child: Center(child: Text("Gestures")),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+
+                  /* Material  Button*/
+
+                  MaterialButton(
+                    onPressed: () {
+                      print(
+                        "I am materialButton",
+                      );
+                    },
+                    splashColor: Colors.deepOrange,
+                    hoverColor: Colors.indigoAccent,
+                    // highlightColor: Colors.lightGreen,
+                    elevation: 20,
+                    height: 40,
+                    child: Text("Material Button "),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    color: Colors.tealAccent,
+                  ),
+                  
+                  /* Outline Button*/
+
+                  OutlineButton(
+                    onPressed: () {
+                      print(
+                        "I am OutlineButton",
+                      );
+                    },
+                    splashColor: Colors.deepOrange,
+                    hoverColor: Colors.indigoAccent,
+                    // highlightColor: Colors.lightGreen,
+                    child: Text("Outline Button "),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    color: Colors.tealAccent,
+                  )
+
+                ],
               ),
-              SizedBox(height: 8),
-
-              /* GestureDetector Button*/
-
-              GestureDetector(
-                onTap: () {
-                  print("I am Tapped GestureDetector");
-                },
-                onHorizontalDragEnd: (value) => print(value),
-                onVerticalDragDown: (value) => print(value),
-                child: Container(
-                  height: 70,
-                  width: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.tealAccent, shape: BoxShape.circle),
-                  child: Center(child: Text("Gestures")),
-                ),
-              ),
-              SizedBox(height: 5),
-
-              /* Material  Button*/
-
-              MaterialButton(onPressed: (){print("I am materialButton",);},
-              splashColor: Colors.deepOrange,
-              hoverColor: Colors.indigoAccent,
-              // highlightColor: Colors.lightGreen,
-              elevation: 20,
-              height: 40,
-              child: Text("Material Button "),
-               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
-                color: Colors.tealAccent,),
-              
-            ],
+            ),
           ),
         ),
 
@@ -173,25 +199,28 @@ class MyApp extends StatelessWidget {
           icon: Icon(Icons.thumb_up),
           backgroundColor: Colors.pink,
         ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
 
-         floatingActionButtonLocation:FloatingActionButtonLocation.startDocked,
+        /*   persistentFooter Button  */
 
-         persistentFooterButtons: [
-           
+        persistentFooterButtons: [
           RaisedButton(
-            onPressed: () {print("I Am Persistant Footer Button");},
+            onPressed: () {
+              print("I Am Persistant Footer Button");
+            },
             color: Colors.greenAccent,
             elevation: 10,
             child: Icon(Icons.print, color: Colors.indigoAccent),
           ),
           RaisedButton(
-            onPressed: () {print("I Am Persistant Footer Button");},
+            onPressed: () {
+              print("I Am Persistant Footer Button");
+            },
             color: Colors.greenAccent,
             elevation: 10,
             child: Icon(Icons.privacy_tip, color: Colors.indigoAccent),
           ),
         ],
-
       ),
     );
   }
