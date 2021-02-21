a71676dbf771fd719fd9c8b0a0dc4a7e5d5d09bd
JITESH SINGH <Jitust98@gmail.com>
2021-02-21 20:17:03 +0530

    Add TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,21 +6,52 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "FlutyLearn",
-      theme: ThemeData(
-        backgroundColor: Colors.deepPurple
-        ,fontFamily: "cursive",
-        ),
+      color: Colors.black,
       home: MyApp(),
     ),
   );
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+  List<Widget> containers = [
+    Center(
+      child: Text("Recieved Message"),
+    ),
+    Center(
+      child: Text("Send Message"),
+    ),
+    Center(
+      child: Text("Delete Message"),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("FlutyLearn"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Tab Bar"),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.message),
+              ),
+              Tab(
+                icon: Icon(Icons.send),
+              ),
+              Tab(
+                icon: Icon(Icons.delete),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: containers,
+          dragStartBehavior: DragStartBehavior.start,
+        ),
       ),
     );
   }
